89e0c4d82e402d141f076f1b77b94953ce4e6086
abhi <abhi@abhis-MacBook-Air.local>
2022-09-01 14:54:11 +0530

    Navbar Fix

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,9 +5,12 @@ function Navbar() {
   return (
     <div><nav className="navbar navbar-expand-lg navbar-dark bg-dark">
     <a className="navbar-brand" href="/">CRUDapp</a>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button> 
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item active">
         <Link className="nav-link" to="/createPost">Create Post</Link>
       </li>
       </ul></div>
